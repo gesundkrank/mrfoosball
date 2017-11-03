2404646ee246afa28fdc93063233773e9d295c85
Jan Grassegger <jan@mbr-targeting.com>
2017-11-03 14:07:31 +0100

    Add help command.

--- src/main/java/eu/m6r/kicker/slack/models/Message.java
+++ src/main/java/eu/m6r/kicker/slack/models/Message.java
@@ -1,10 +1,23 @@
 package eu.m6r.kicker.slack.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Message {
-    public Message() {}
+
+    public String channel;
+    public String type;
+    public String text;
+    public String user;
+    public boolean as_user;
+    public List<Attachment> attachments = new ArrayList<>();
+
+    public Message() {
+    }
 
     public Message(String channel, String text, String user) {
         this.channel = channel;
@@ -13,8 +26,46 @@ public class Message {
         this.user = user;
     }
 
-    public String channel;
-    public String type;
-    public String text;
-    public String user;
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Attachment {
+
+        public String title;
+        public String text;
+        public String color = "#ff0080";
+
+        public List<String> mrkdwn_in = new ArrayList<>();
+        public List<Field> fields = new ArrayList<>();
+
+        public Attachment() {
+        }
+
+        public Attachment(final String title, final String text) {
+            this.title = title;
+            this.text = text;
+            mrkdwn_in.add("text");
+            mrkdwn_in.add("fields");
+        }
+
+        public static class Field {
+
+            public String title;
+            public String value;
+
+            @JsonProperty(value = "short")
+            public boolean isShort = false;
+
+            public Field() {
+
+            }
+
+            public Field(String value) {
+                this.value = value;
+            }
+
+            public Field(String title, String value) {
+                this.title = title;
+                this.value = value;
+            }
+        }
+    }
 }

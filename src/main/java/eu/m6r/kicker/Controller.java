99c453d025e622d67d9821cbf741812f2c7d7d05
Jan Grassegger <jan@mbr-targeting.com>
2018-01-05 14:47:29 +0100

    Clean finished tournament.

--- src/main/java/eu/m6r/kicker/Controller.java
+++ src/main/java/eu/m6r/kicker/Controller.java
@@ -91,6 +91,7 @@ public class Controller {
             store.saveTournament(updatedTournament);
         }
 
+        this.runningTournament.clear();
     }
 
     public List<Tournament> getTournaments() {

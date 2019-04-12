62398f5a7f2cd8860d0b45e116ec1f52b8fc6bb5
Jan Grassegger <jan@mbr-targeting.com>
2019-04-12 13:36:07 +0200

    Add GPL license.

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -1,4 +1,21 @@
 /*
+ * This file is part of kicker (https://github.com/mbrtargeting/kicker).
+ * Copyright (c) 2019 Jan Graßegger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  Declaration files are how the Typescript compiler knows about the type information(or shape) of an object.
  They're what make intellisense work and make Typescript know all about your code.
 

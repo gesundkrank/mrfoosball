d2db5f34261c3d575ea76efc738e33b501a90779
Jan Grassegger <jan@mbr-targeting.com>
2019-04-16 17:25:11 +0200

    Use teamSkill for matchmaking.

--- src/main/java/eu/m6r/kicker/store/Store.java
+++ src/main/java/eu/m6r/kicker/store/Store.java
@@ -125,6 +125,10 @@ public class Store implements Closeable {
     }
 
     public Team getTeam(final Player player1, final Player player2) {
+        return getTeam(player1, player2, true);
+    }
+
+    public Team getTeam(final Player player1, final Player player2, boolean persist) {
         Transaction tx = session.beginTransaction();
 
         session.saveOrUpdate(player1);
@@ -145,7 +149,9 @@ public class Store implements Closeable {
             team = new Team();
             team.player1 = key.player1;
             team.player2 = key.player2;
-            session.save(team);
+            if (persist) {
+                session.save(team);
+            }
         }
 
         tx.commit();

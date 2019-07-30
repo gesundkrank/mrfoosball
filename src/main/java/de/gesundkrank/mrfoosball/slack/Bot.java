efc3c0368af27a729a846338bd98e8b5ff3e3163
Roman Orzegowski <roman@mbr-targeting.com>
2019-07-30 17:44:35 +0200

    Add ability to join private channels

--- src/main/java/de/gesundkrank/mrfoosball/slack/Bot.java
+++ src/main/java/de/gesundkrank/mrfoosball/slack/Bot.java
@@ -149,10 +149,12 @@ public class Bot {
     public void onMessage(final String messageString, final Session session) throws IOException {
         logger.info(messageString);
 
-        if (messageString.contains("\"type\":\"channel_joined\"")) {
+        if (messageString.contains("\"type\":\"channel_joined\"")
+            || messageString.contains("\"type\":\"group_joined\"")) {
 
             final var channelJoined =
                     jsonConverter.fromString(messageString, ChannelJoined.class);
+
             final var id = controller
                     .joinChannel(channelJoined.channel.id, channelJoined.channel.name);
             sendChannelJoinedMessage(channelJoined.channel.id, id);
